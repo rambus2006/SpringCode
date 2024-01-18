a2fb735501f954a9a3ce0b6eb1c221000c3e1385
rambus2006 <bangminseo50@gmail.com>
2024-01-18 23:41:15 +0900

    AppConfig.java

--- AppConfig.java
+++ AppConfig.java
@@ -1,4 +1,5 @@
 package hello.core;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
@@ -11,22 +12,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                memberRepository(),
-                discountPolicy());
+        System.out.println("call AppConfig.memberService");
+
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberService");
         return new MemoryMemberRepository();
     }
     @Bean
     public DiscountPolicy discountPolicy() {
+        System.out.println("call AppConfig.memberService");
         return new RateDiscountPolicy();
     }
 }
